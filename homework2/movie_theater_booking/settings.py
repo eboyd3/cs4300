611f49af9b40602d6c3ad869d65608d699c5877a
eboyd3 <eboyd3@uccs.edu>
2026-02-27 22:30:15 +0000

    Adding stuff to use with Render. We shall see if this works

--- homework2/movie_theater_booking/settings.py
+++ homework2/movie_theater_booking/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,14 +26,14 @@ SECRET_KEY = 'django-insecure-el-@eqe8-xqhfc-h*iy#u5cdqa3zh@+$5a)#dk&ki4&2h(4yk)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['editor-eboyd3-21.devedu.io']
-
+ALLOWED_HOSTS = ['editor-eboyd3-21.devedu.io', 'app-eboyd3-21.devedu.io','*']
 
 # Application definition
 
 INSTALLED_APPS = [
     'bookings.apps.BookingsConfig',
     'rest_framework',
+    'django_behave',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -117,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
